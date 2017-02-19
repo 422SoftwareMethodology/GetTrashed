1eda9d08173a0cf7e2833f3b4cb47ba88de0be9f
Jordan Merckling <jmerckli@uoregon.edu>
2017-02-18 18:38:01 -0800

    Original GUI

---
+++ src/application/Main.java
@@ -0,0 +1,29 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+//import javafx.scene.layout.BorderPane;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			//BorderPane root = new BorderPane();
+			Parent root = FXMLLoader.load(getClass().getResource("/TrashedGUI.fxml"));
+			Scene scene = new Scene(root,620,400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
