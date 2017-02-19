a64bb016e9588f10984d66e6d4ddb428fcf32f5f
Craig Totten <craigt@uoregon.edu>
2017-02-18 23:31:50 -0800

    Two classes boi

---
+++ src/Driver.java
@@ -0,0 +1,12 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Driver extends Application {
+		void launch() {}
+
+		@Override
+		public void start(Stage arg0) throws Exception {
+			// TODO Auto-generated method stub
+			
+		}
+	}
